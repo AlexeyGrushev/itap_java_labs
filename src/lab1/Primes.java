041ff639958713990560ea1a1cb7f7e95e2ca402
AlexeyGrushev <yablokov2004@mail.ru>
2025-09-22 17:13:40 +0300

    Initial commit: added lab1

---
+++ src/lab1/Primes.java
@@ -0,0 +1,21 @@
+package lab1;
+public class Primes {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
